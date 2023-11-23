6e284b100a719b9247267db0df5136fccb364254
Chahine <tchamitouche@gmail.com>
2023-11-23 16:53:24 -0500

    Remade la page d'Accueil + added tables for DB + ajout rapport

---
+++ database/bdschema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS Services (
+    idService 				INT PRIMARY KEY,
+    nomService 				VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Medecins (
+    idMedecin 				INT PRIMARY KEY,
+    prenom 					VARCHAR(50),
+    nom 					VARCHAR(50),
+    specialite 				VARCHAR(50),
+    anneesExperience 		INT,
+    idService 				INT,
+    FOREIGN KEY (idService) REFERENCES Services(idService)
+);
