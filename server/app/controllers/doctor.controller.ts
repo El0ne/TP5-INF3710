f9f86a38b9933d73f2b4b30c1278139b31e33e88
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-11-27 19:02:03 -0500

    added delete and put

--- server/app/controllers/doctor.controller.ts
+++ server/app/controllers/doctor.controller.ts
@@ -55,13 +55,32 @@ export class DoctorController {
 				.catch((error) => console.error("Error executing query", error.stack));
 		});
 
-		this.router.post("/", (req, res, _) => {
+		this.router.post("/", (req: Request, res: Response) => {
 			const doctor: Doctor = req.body;
-			console.log(doctor);
+			const sqlQuery = `INSERT INTO public.medecins(idMedecin, prenom, nom, specialite, anneesExperience, idService) 
+                              VALUES ($1, $2, $3, $4, $5, $6)`;
+			this.databaseService.pool
+				.query(sqlQuery, [
+					doctor.id,
+					doctor.firstName,
+					doctor.lastName,
+					doctor.specialization,
+					doctor.yoe,
+					doctor.serviceId,
+				])
+				.then(() => {
+					res.send(doctor);
+				})
+				.catch((error) => console.error("Error executing query", error.stack));
+		});
+
+		this.router.put("/", (req: Request, res: Response) => {
+			const doctor: Doctor = req.body;
 			this.databaseService.pool
 				.query(
-					`INSERT INTO public.medecins(idMedecin, prenom, nom, specialite, anneesExperience, idService) 
-        VALUES ($1, $2, $3, $4, $5, $6)`,
+					`UPDATE public.medecins m 
+                    SET prenom = $2, nom = $3, specialite = $4, anneesExperience = $5, idService = $6
+                    WHERE m.idMedecin = $1`,
 					[
 						doctor.id,
 						doctor.firstName,
@@ -71,8 +90,21 @@ export class DoctorController {
 						doctor.serviceId,
 					]
 				)
-				.then((response) => {
-					res.send(response.rows);
+				.then(() => {
+					res.send(doctor);
+				})
+				.catch((error) => console.error("Error executing query", error.stack));
+		});
+
+		this.router.delete("/:id", async (req: Request, res: Response) => {
+			const sqlQuery = `DELETE FROM public.medecins WHERE idMedecin = $1`;
+			this.databaseService.pool
+				.query({
+					text: sqlQuery,
+					values: [req.params.id],
+				})
+				.then(() => {
+					res.send([]);
 				})
 				.catch((error) => console.error("Error executing query", error.stack));
 		});
